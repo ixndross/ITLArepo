122930691fc978d7898731e802a8d0722821c18b
IxnDross <ixndross@gmail.com>
2025-03-18 20:18:04 -0400

    Prog1_CSharp_Tarea2_AlexanderVoskericyan_20241119_Viernes

---
+++ Tareas/ParityCheck/Prog1_CSharp_Tarea2_AlexanderVoskericyan_20241119_Viernes.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Por favor, introduzca un numeero para verificar su paridad: ");
+
+double doBle = Convert.ToDouble(Console.ReadLine());
+
+if (doBle % 2 == 0)
+{
+    Console.WriteLine($"El número {doBle} es par.");
+}
+else
+{
+    Console.WriteLine($"El número {doBle} es impar.");
+}
+
+Console.WriteLine("Presione cualquier tecla para cerrar el programa.");
+
+Console.ReadKey();
