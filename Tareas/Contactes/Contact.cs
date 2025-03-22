cc181d5ceab18b09d492b5e6b133d763c2b27604
IxnDross <ixndross@gmail.com>
2025-03-22 01:38:39 -0400

    Tarea6_Prog1_CSharp_AlexanderVoskericyan20241119_Viernes

---
+++ Tareas/Contactes/Contact.cs
@@ -0,0 +1,34 @@
+﻿namespace Contactes
+{
+    public class Contact
+    { 
+        public Contact()
+        {
+            //Constructor vacio para obtener sobrecarga de metodos y permitir la creacion de contactos sin argumentos.
+        }
+        
+        public Contact(int id, string name, string lastname, string phone, string email, string address, int age, bool isFavorite)
+        {
+            ID = id;
+            Name = name;
+            LastName = lastname;
+            Phone = phone;
+            Email = email;
+            Address = address;
+            Age = age;
+            IsFavorite = isFavorite;
+
+            //Constructor base
+        }
+
+        public int ID { get;  set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; } 
+        public bool IsFavorite { get; set; }
+
+    }
+}
---
+++ Tareas/Contactes/ContactesClassEstructurada.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel;
+using System.Linq;
+using System.Timers;
+using ContactosOOP;
+
+Console.WriteLine("Bienvenido a la lista de contactos.");
+
+bool runing = true;
+
+List<Contact> Contactos = new List<Contact>();
+
+while (runing)
+{
+    try
+    {
+        HeaderHelpers.Selection();
+
+        int typeOption = Convert.ToInt32(Console.ReadLine());
+
+        switch (typeOption)
+        {
+            case 1:
+                {
+                    HeaderHelpers.AddContact(Contactos);
+
+                }
+                break;
+            case 2:
+                {
+                    HeaderHelpers.ShowContacts(Contactos);
+                }
+                break;
+            case 3:
+                {
+                    HeaderHelpers.SearchContacts(Contactos);
+                }
+                break;
+            case 4:
+                {
+                    HeaderHelpers.ModifyContacts(Contactos);
+                }
+                break;
+            case 5:
+                {
+                    HeaderHelpers.DeleteContacts(Contactos);
+                }
+                break;
+            case 6:
+                {
+                    Console.WriteLine("Cerrando el programa...");
+                    runing = false;
+                    break;
+                }
+            default:
+
+                Console.WriteLine("Favor introducir una entrada valida");
+                break;
+        }
+    }
+    catch (NullReferenceException)
+    {
+        Console.WriteLine("El contacto seleccionado no existe.\n ");
+    }
+    catch (IndexOutOfRangeException)
+    {
+        Console.WriteLine("El programa ha encontrado un error buscando este dato. Por favor intente de nuevo.\n");
+    }
+}
+
+
+
+
